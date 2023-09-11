9e4da4e763eff95c37162e1939c768259d13d1a3
Bealby <mgbealby@gmail.com>
2023-09-11 20:59:46 +0200

    Added product page view and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
 
-# Create your views here.
+from .models import Product, Category
+from .forms import ProductForm
+
+
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
