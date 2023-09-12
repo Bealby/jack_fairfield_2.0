64d680fa67fc92721ce19f51773d91e2a832b1ce
Bealby <mgbealby@gmail.com>
2023-09-12 15:48:44 +0200

    Added bag context

--- bag/views.py
+++ bag/views.py
@@ -10,3 +10,23 @@ def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    # Add product to shopping bag
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    # To ensure only one unique product is added to bag
+    if item_id in list(bag.keys()):
+        messages.warning(request, f'{product.name} is already in your bag')
+    else:
+        bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
+
+    request.session['bag'] = bag
+    # Redirect user to previous page
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
--- jack_fairfield/settings.py
+++ jack_fairfield/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',  # Required by allauth. Allows access to http request object. Allows access user etc
                 'django.contrib.messages.context_processors.messages',
+                'basket.contexts.basket_contents',
             ],
         },
     },
@@ -149,6 +150,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
